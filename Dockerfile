4297a5bd6dd86dc7245c2d2cf09c4fd053fa4f9c
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2024-03-26 16:04:50 -0300

    .

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10.13-alpine
+
+WORKDIR ./teste
+COPY ../src ./src
+COPY ../pyproject.toml ./
+COPY ../poetry.lock ./
+COPY ../README.md ./
+
+RUN pip install --upgrade pip
+RUN pip install -U pip setuptools
+RUN pip install poetry
+RUN poetry config installer.max-workers 10
+RUN poetry install --no-interaction
+
+CMD ["poetry", "run", "python", "src/connection.py"]
+
+# docker build -t radar-do-congresso -f ./docker/Dockerfile .
+# docker run radar-do-congresso
---
+++ src/connection.py
@@ -0,0 +1,63 @@
+import os
+from google.cloud import storage
+from loguru import logger
+import mysql.connector
+
+
+def upload_blob(
+    bucket_name, source_file_name, destination_blob_name
+):  # pylint: disable=C0116
+    try:
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+
+        blob.upload_from_filename(source_file_name)
+
+        logger.info(f"File {source_file_name} uploaded to {destination_blob_name}.")
+    except Exception as e:  # pylint: disable=W0718
+        logger.exception(e)
+
+
+def list_buckets():  # pylint: disable=C0116
+    try:
+        client = storage.Client()
+        buckets = list(client.list_buckets())
+        logger.info("Conexão bem-sucedida. Baldes disponíveis:")
+        for bucket in buckets:
+            logger.info(bucket.name)
+    except Exception as e:  # pylint: disable=W0718
+        logger.error("Falha ao conectar ao Cloud Storage:", e)
+
+
+def test_mysql_connection(
+    host, user, password, database
+):  # pylint: disable=W0621,C0116
+    try:
+        connection = mysql.connector.connect(
+            host=host, user=user, password=password, database=database
+        )
+
+        if connection.is_connected():
+            logger.info("Conexão bem-sucedida ao banco de dados MySQL!")
+            cursor = connection.cursor()
+            cursor.execute("SHOW TABLES;")
+            tables = cursor.fetchall()
+            logger.info("Tabelas disponíveis:")
+            for table in tables:
+                logger.info(table[0])
+            cursor.close()
+            connection.close()
+    except Exception as e:  # pylint: disable=W0718
+        logger.exception(e)
+
+
+if __name__ == "__main__":
+    list_buckets()
+    test_mysql_connection(
+        host=os.getenv(""),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+    )
+    upload_blob(bucket_name="infograficos", source_file_name="./content.txt", destination_blob_name="content.txt")
